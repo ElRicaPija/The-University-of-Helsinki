a4f7684df58bed88f03bf2176167384a59d5e237
ElRicaPija <rl0051844@gmail.com>
2024-07-30 13:09:50 -0700

    UPDATE-Correcion de componente de contenido y creacion de Componente Total

--- part1/infocurso/src/App.jsx
+++ part1/infocurso/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 
 const Header = () => {
   const curso = 'Desarrollo de una aplicacion usando React + Vite'
+  console.log(curso)
   return (
     <div>
       <h1>{curso}</h1>
@@ -12,6 +13,30 @@ const Header = () => {
 }
 
 const Contenido = (props) => {
+  console.log(props.parte1,props.ejercicios)
+  console.log(props.parte2,props.ejercicios2)
+  console.log(props.parte3,props.ejercicios3)
+  return (
+    <div>
+      <p>{props.parte1} {props.ejercicios}</p>
+      <p>{props.parte2} {props.ejercicios2}</p>
+      <p>{props.parte3} {props.ejercicios3}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const total = 'El total de ejercicios son :'
+  console.log(total)
+  console.log(props.ejercicios + props.ejercicios2 + props.ejercicios3)
+  return (
+    <div>
+      <p>{total}{props.ejercicios + props.ejercicios2 + props.ejercicios3}</p>
+    </div>
+  )
+}
+
+const App = () => {
   const parte1 = 'Fundamentos de React'
   const ejercicios = 10
   const parte2 = 'Usando props para pasar la informacion'
@@ -20,18 +45,20 @@ const Contenido = (props) => {
   const ejercicios3 = 14 
   return (
     <div>
-      <p>{parte1} {ejercicios}</p>
-      <p>{parte2} {ejercicios2}</p>
-      <p>{parte3} {ejercicios3}</p>
-    </div>
-  )
-}
-
-const App = () => {
-  return (
-    <div>
       <Header />
-      <Contenido />
+      <Contenido 
+        parte1 = {parte1}
+        ejercicios = {ejercicios}
+        parte2 = {parte2}
+        ejercicios2 = {ejercicios2}
+        parte3 = {parte3}
+        ejercicios3 = {ejercicios3} 
+      />
+      <Total 
+        ejercicios = {ejercicios}
+        ejercicios2 = {ejercicios2}
+        ejercicios3 = {ejercicios3}
+      />
     </div>
   )
 }
