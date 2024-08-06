ce698ae6374b54f5d8da728438cdda2adb15fe46
ElRicaPija <rl0051844@gmail.com>
2024-08-06 16:21:21 -0700

    FEATURE-Estudio de Funciones auxiliares del componente

--- part1/contenidoEstudio/src/App.jsx
+++ part1/contenidoEstudio/src/App.jsx
@@ -3,32 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Hello = ({nombre, edad}) => {
+  console.log('Tu nombre es :',nombre);
+  console.log('Tu edad es :',edad);
+  return (
+    <div>
+      <p>
+        Hola {nombre}, tu tienes {edad} años de edad
+      </p>
+    </div>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const nombre = 'Ricardo'
+  const edad = 20
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Saludos</h1>
+      <Hello nombre={nombre} edad={edad} />
+      <Hello nombre="Ulises" edad={26 + 10} />
+    </div>
   )
 }
 
